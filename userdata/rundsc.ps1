1715d997d325bb4c30d413873b84000c3dcd1123
markcor <mcornmesser@mozilla.com>
2017-03-02 15:45:06 -0800

    Update rundsc.ps1

--- userdata/rundsc.ps1
+++ userdata/rundsc.ps1
@@ -374,7 +374,6 @@ Write-host "$SourceRepo"
 
 # The Windows update service needs to be enabled for OCC to process but needs to be disabled during testing. 
 $UpdateService = Get-Service -Name wuauserv
-Set-Service $UpdateService -StartupType Manual
 if ($UpdateService.Status -ne "Running"){
  Start-Service $UpdateService
  Write-Log -message 'Enabling Windows update service'

96902971de3b00abea6f75e47a3533a08cdd3b73
markcor <mcornmesser@mozilla.com>
2017-03-22 08:24:13 -0700

    Update DynamicConfig.ps1

--- userdata/DynamicConfig.ps1
+++ userdata/DynamicConfig.ps1
@@ -41,7 +41,7 @@ Configuration DynamicConfig {
     Ensure = 'Present'
   }
   If ($LocationType -eq "DataCenter" ) {
-  	Copy-Item "C:\programdata\occ-installers.tok" "C:\builds\"
+  	Copy-Item "C:\programdata\occ-installers.tok" "C:\builds\occ-installers.tok"
 	}
   If ($LocationType -eq "AWS") { 
     Script FirefoxBuildSecrets {

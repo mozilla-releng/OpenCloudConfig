dc09ae45e9b05fbfd12f910de9460f1bd5a3920f
markcor <mcornmesser@mozilla.com>
2017-04-03 15:51:53 -0700

    Update DynamicConfig.ps1

--- userdata/DynamicConfig.ps1
+++ userdata/DynamicConfig.ps1
@@ -7,8 +7,7 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 Configuration DynamicConfig {
   Import-DscResource -ModuleName PSDesiredStateConfiguration
   # SourceRepo is in place to toggle between production and testing environments
-  # $SourceRepo = "mozilla-releng"
-  $SourceRepo = "markcor"
+  $SourceRepo = "mozilla-releng"
     
   if (Get-Service "Ec2Config" -ErrorAction SilentlyContinue) {
     $LocationType = "AWS"

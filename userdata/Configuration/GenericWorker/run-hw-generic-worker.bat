bfa73ccbe3f05597dd8b8c99c73c814633545f43
markcor <mcornmesser@mozilla.com>
2017-04-13 11:38:59 -0700

    Add files via upload

---
+++ userdata/Configuration/GenericWorker/run-hw-generic-worker.bat
@@ -0,0 +1,13 @@
+
+
+if exist C:\generic-worker\disable-desktop-interrupt.reg reg import C:\generic-worker\disable-desktop-interrupt.reg
+
+:CheckForStateFlag
+if exist C:\dsc\task-claim-state.valid goto RunWorker
+timeout /t 1 >nul
+goto CheckForStateFlag
+
+:RunWorker
+del /Q /F C:\dsc\task-claim-state.valid
+pushd %~dp0
+.\generic-worker.exe run run --config C:\generic-worker\work.config

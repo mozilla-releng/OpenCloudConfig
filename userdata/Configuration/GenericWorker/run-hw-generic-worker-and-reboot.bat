b4b735ad457553ef22b14dddfc35fe371125a0b1
markcor <mcornmesser@mozilla.com>
2017-05-08 10:07:55 -0700

    Update run-hw-generic-worker-and-reboot.bat

--- userdata/Configuration/GenericWorker/run-hw-generic-worker-and-reboot.bat
+++ userdata/Configuration/GenericWorker/run-hw-generic-worker-and-reboot.bat
@@ -10,7 +10,6 @@ goto CheckForStateFlag
 :RunWorker
 del /Q /F C:\dsc\task-claim-state.valid
 pushd %~dp0
-rem C:\generic-worker\generic-worker.exe run --config C:\generic-worker\gen_worker.config > C:\generic-worker\generic-worker.log 2>&1
-C:\generic-worker\generic-worker.exe run --config C:\generic-worker\gen_worker.config
+C:\generic-worker\generic-worker.exe run --config C:\generic-worker\gen_worker.config > C:\generic-worker\generic-worker.log 2>&1
 
-rem shutdown /r /t 0 /f /c "Rebooting as generic worker ran successfully"
+shutdown /r /t 0 /f /c "Rebooting as generic worker ran successfully"

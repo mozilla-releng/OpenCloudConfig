1f19cb505e80ad5c413b1a88039422dbe9dfa560
Rob Thijssen <rthijssen@gmail.com>
2018-09-27 15:39:26 +0300

    Bug 1493744 - require key bit length of 4096

--- ci/validate_rsa_keys.sh
+++ ci/validate_rsa_keys.sh
@@ -2,7 +2,7 @@
 
 while read pub_key; do
   rsa_bit_length=$(echo ${pub_key} | ssh-keygen -lf - | { read -a i; echo ${i[0]}; })
-  if [ ${rsa_bit_length} -lt 2048 ]; then
+  if [ ${rsa_bit_length} -lt 4096 ]; then
     (>&2 echo "key (${pub_key##* }) with rsa bit length of ${rsa_bit_length} is rejected.")
     exit 1
   fi

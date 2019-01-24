5c575c00a3fee4c410860ff05af00a999415021e
Rob Thijssen <rthijssen@gmail.com>
2019-01-24 16:49:35 +0200

    Bug 1512090 - log component: ReplaceInFile output
    deploy: beta
    
    beta-source: mozilla-releng OpenCloudConfig beta

--- userdata/xDynamicConfig.ps1
+++ userdata/xDynamicConfig.ps1
@@ -930,8 +930,14 @@ Configuration xDynamicConfig {
           DependsOn = @( @($item.DependsOn) | ? { (($_) -and ($_.ComponentType)) } | % { ('[{0}]{1}_{2}' -f $componentMap.Item($_.ComponentType), $_.ComponentType, $_.ComponentName) } )
           GetScript = "@{ ReplaceInFile = $item.ComponentName }"
           SetScript = {
-            $content = ((Get-Content -Path $using:item.Path) | Foreach-Object { $_ -replace $using:item.Match, (Invoke-Expression -Command $using:item.Replace) })
-            [System.IO.File]::WriteAllLines($using:item.Path, $content, (New-Object System.Text.UTF8Encoding $false))
+            try {
+              $content = ((Get-Content -Path $using:item.Path) | Foreach-Object { $_ -replace $using:item.Match, (Invoke-Expression -Command $using:item.Replace) })
+              [System.IO.File]::WriteAllLines($using:item.Path, $content, (New-Object System.Text.UTF8Encoding $false))
+              Write-EventLog -LogName 'Application' -Source 'occ-dsc' -EntryType 'Information' -EventId 1 -Message ('{0} :: replaced occurences of: {1} with: {2} in: {3}' -f $using:item.ComponentName, $using:item.Match, $using:item.Replace, $using:item.Path)
+            } catch {
+              Write-EventLog -LogName 'Application' -Source 'occ-dsc' -EntryType 'Error' -EventId 9 -Message ('{0} :: failed to replace occurences of: {1} with: {2} in: {3}. {4}' -f $using:item.ComponentName, $using:item.Match, $using:item.Replace, $using:item.Path, $_.Exception.Message)
+              throw
+            }
           }
           TestScript = { return $false }
         }

6598463cdf480874be5f90395ea67a30c24942bc
Rob Thijssen <rthijssen@gmail.com>
2019-01-28 16:38:18 +0200

    Bug 1522762 - use corrected occ module
    deploy: beta
    
    beta-source: mozilla-releng OpenCloudConfig beta

--- userdata/OCC-Bootstrap.psm1
+++ userdata/OCC-Bootstrap.psm1
@@ -101,7 +101,7 @@ function Invoke-RemoteDesiredStateConfig {
       @{
         'ModuleName' = 'OpenCloudConfig';
         'Repository' = 'PSGallery';
-        'ModuleVersion' = '0.0.1'
+        'ModuleVersion' = '0.0.2'
       }
     )
   )

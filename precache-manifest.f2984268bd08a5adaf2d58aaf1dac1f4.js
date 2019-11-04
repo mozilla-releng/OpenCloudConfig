b4b483c214b8141fb8f60dffff8392a2bf82c196
Rob Thijssen <rthijssen@gmail.com>
2019-11-04 18:01:28 +0200

    Updates

---
+++ precache-manifest.f2984268bd08a5adaf2d58aaf1dac1f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825797a9c56efb7bb5912c68f661df3b",
+    "url": "/github.io/OpenCloudConfig/index.html"
+  },
+  {
+    "revision": "9fb52b1c966226ab2bee",
+    "url": "/github.io/OpenCloudConfig/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9f16ed50ae30cd0a9442",
+    "url": "/github.io/OpenCloudConfig/static/js/2.21f0d059.chunk.js"
+  },
+  {
+    "revision": "9fb52b1c966226ab2bee",
+    "url": "/github.io/OpenCloudConfig/static/js/main.6abb60e8.chunk.js"
+  },
+  {
+    "revision": "3de5609eaf2ff64bde8a",
+    "url": "/github.io/OpenCloudConfig/static/js/runtime-main.350be4ae.js"
+  }
+]);

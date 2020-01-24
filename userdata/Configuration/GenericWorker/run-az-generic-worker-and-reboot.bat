073d5b0ce4e707bc0e46756d4e77b4cabc7ccf01
Rob Thijssen <rthijssen@gmail.com>
2020-01-24 08:59:10 +0200

    switch back to not using runner on azure

--- userdata/Configuration/GenericWorker/run-az-generic-worker-and-reboot.bat
+++ userdata/Configuration/GenericWorker/run-az-generic-worker-and-reboot.bat
@@ -16,12 +16,25 @@ ping -n 2 127.0.0.1 1>/nul
 goto CheckForStateFlag
 
 :RunWorker
+rem set workerId, publicIP, clientId and accessToken in gw.config
+for /f "tokens=14" %%i in ('"ipconfig | findstr IPv4"') do set public_ip=%%i
+for /f "usebackq tokens=2,* skip=2" %%J in (
+  `wmic computersystem get name`
+) do set worker_id=%%K
+for /f "usebackq tokens=2,* skip=2" %%L in (
+  `reg query "HKLM\SOFTWARE\Mozilla\GenericWorker" /v clientId`
+) do set client_id=%%M
+for /f "usebackq tokens=2,* skip=2" %%N in (
+  `reg query "HKLM\SOFTWARE\Mozilla\GenericWorker" /v accessToken`
+) do set access_token=%%O
+cat C:\generic-worker\generic-worker.config | jq ".  | .workerId=\"%worker_id%\" | .rootURL=\"https://stage.taskcluster.nonprod.cloudops.mozgcp.net\" | .clientId=\"%client_id%\" | .accessToken=\"%access_token%\"" > C:\generic-worker\gw.config
+
 echo File C:\dsc\task-claim-state.valid found >> C:\generic-worker\generic-worker-wrapper.log
 echo Deleting C:\dsc\task-claim-state.valid file >> C:\generic-worker\generic-worker-wrapper.log
 del /Q /F C:\dsc\task-claim-state.valid >> C:\generic-worker\generic-worker-wrapper.log 2>&1
 pushd %~dp0
 set errorlevel=
-C:\generic-worker\taskcluster-worker-runner.exe C:\generic-worker\taskcluster-worker-runner.yaml >> .\taskcluster-worker-runner.log 2>&1
+C:\generic-worker\generic-worker.exe run --config C:\generic-worker\gw.config >> .\generic-worker.log 2>&1
 set gw_exit_code=%errorlevel%
 
 rem exit code 67 means generic worker has created a task user and wants to reboot into it

134ac61cabd94caa105d0bb2eb04930b8ae93b21
Rob Thijssen <rthijssen@gmail.com>
2021-03-01 09:02:04 +0200

    bug 1694961 - update beta workers to tc 41.1.0
    ensure y: drive availability
    
    remove redundant gw config
    
    deploy: gecko-1-b-win2012-beta gecko-t-win7-32-beta gecko-t-win7-32-gpu-b gecko-t-win10-64-beta gecko-t-win10-64-gpu-b

--- userdata/OCC-Bootstrap.psm1
+++ userdata/OCC-Bootstrap.psm1
@@ -124,14 +124,14 @@ function Install-Dependencies {
       @{
         'ModuleName' = 'OpenCloudConfig';
         'Repository' = 'PSGallery';
-        'ModuleVersion' = '0.0.56'
+        'ModuleVersion' = '0.0.58'
       }
     ),
     # if modules are detected with a version **less than** specified in ModuleVersion below, they will be purged
     [hashtable[]] $purgeModules = @(
       @{
         'ModuleName' = 'OpenCloudConfig';
-        'ModuleVersion' = '0.0.56'
+        'ModuleVersion' = '0.0.58'
       }
     )
   )
